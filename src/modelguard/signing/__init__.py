7216176fd1f7d4d7028083d150bea62867d049e9
sakthivarshans <email2sakthi.s@gmail.com>
2026-09-16 18:46:59 +0000

    feat(signing): expose signing public API

---
+++ src/modelguard/signing/__init__.py
@@ -0,0 +1,32 @@
+from modelguard.signing.envelope import SignatureEnvelope, SignaturePayload
+from modelguard.signing.keys import (
+    LocalKeyPair,
+    generate_keypair,
+    load_private_key,
+    load_public_key,
+    save_keypair,
+)
+from modelguard.signing.signer import sign_artifact
+from modelguard.signing.verifier import (
+    TamperCheckResult,
+    check_artifact_digest,
+    check_mbom_digest,
+    check_signature_bytes,
+    verify_artifact,
+)
+
+__all__ = [
+    "LocalKeyPair",
+    "SignatureEnvelope",
+    "SignaturePayload",
+    "TamperCheckResult",
+    "check_artifact_digest",
+    "check_mbom_digest",
+    "check_signature_bytes",
+    "generate_keypair",
+    "load_private_key",
+    "load_public_key",
+    "save_keypair",
+    "sign_artifact",
+    "verify_artifact",
+]
