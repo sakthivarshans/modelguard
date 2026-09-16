c2cfe4ab18cd8eb9295783b28f6df5688e3957ae
sakthivarshans <email2sakthi.s@gmail.com>
2026-09-16 18:46:59 +0000

    feat(hashing): expose hashing public API

---
+++ src/modelguard/hashing/__init__.py
@@ -0,0 +1,15 @@
+from modelguard.hashing.digest import (
+    ArtifactDigest,
+    FileDigest,
+    hash_artifact,
+    hash_directory,
+    hash_file,
+)
+
+__all__ = [
+    "ArtifactDigest",
+    "FileDigest",
+    "hash_artifact",
+    "hash_directory",
+    "hash_file",
+]
