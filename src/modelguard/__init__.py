5a741e3af9f737713e1bdf1667e60574c3f7d6aa
sakthivarshans <email2sakthi.s@gmail.com>
2026-09-17 08:43:14 +0000

    chore: bump __version__ to 0.3.0

--- src/modelguard/__init__.py
+++ src/modelguard/__init__.py
@@ -9,7 +9,7 @@ implemented.
 from modelguard.exceptions import ModelGuardError, VerificationDenied
 from modelguard.sdk import ModelGuard, VerificationResult
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 __all__ = [
     "ModelGuard",
