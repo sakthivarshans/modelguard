dd8a79607e02a3fd43797a44b67dd376e8f78084
sakthivarshans <email2sakthi.s@gmail.com>
2026-09-19 13:51:29 +0000

    feat(signing): export trust helpers

--- src/modelguard/signing/__init__.py
+++ src/modelguard/signing/__init__.py
@@ -7,6 +7,7 @@ from modelguard.signing.keys import (
     save_keypair,
 )
 from modelguard.signing.signer import sign_artifact
+from modelguard.signing.trust import normalize_fingerprints, public_key_fingerprint
 from modelguard.signing.verifier import (
     TamperCheckResult,
     check_artifact_digest,
@@ -26,6 +27,8 @@ __all__ = [
     "generate_keypair",
     "load_private_key",
     "load_public_key",
+    "normalize_fingerprints",
+    "public_key_fingerprint",
     "save_keypair",
     "sign_artifact",
     "verify_artifact",
