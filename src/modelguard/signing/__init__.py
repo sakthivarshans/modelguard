9120ab95ec64e459aa165ac98c22c5d99da5d076
sakthivarshans <email2sakthi.s@gmail.com>
2026-09-22 18:36:13 +0000

    feat(signing): export the new scheme, provider, and verification APIs

--- src/modelguard/signing/__init__.py
+++ src/modelguard/signing/__init__.py
@@ -1,4 +1,8 @@
-from modelguard.signing.envelope import SignatureEnvelope, SignaturePayload
+from modelguard.signing.envelope import (
+    SignatureEnvelope,
+    SignaturePayload,
+    load_envelope,
+)
 from modelguard.signing.keys import (
     LocalKeyPair,
     generate_keypair,
@@ -6,30 +10,53 @@ from modelguard.signing.keys import (
     load_public_key,
     save_keypair,
 )
-from modelguard.signing.signer import sign_artifact
+from modelguard.signing.providers import LocalEd25519Signer, SignerProvider
+from modelguard.signing.schemes import (
+    ALGORITHM_ECDSA_P256_SHA256,
+    ALGORITHM_ED25519,
+    DEFAULT_VERIFIERS,
+    SignatureVerifier,
+    key_fingerprint,
+    verifier_registry,
+)
+from modelguard.signing.signer import sign_artifact, sign_with_provider
 from modelguard.signing.trust import normalize_fingerprints, public_key_fingerprint
 from modelguard.signing.verifier import (
+    SignatureCheck,
     TamperCheckResult,
     check_artifact_digest,
     check_mbom_digest,
     check_signature_bytes,
     verify_artifact,
+    verify_envelope_signature,
 )
 
 __all__ = [
+    "ALGORITHM_ECDSA_P256_SHA256",
+    "ALGORITHM_ED25519",
+    "DEFAULT_VERIFIERS",
+    "LocalEd25519Signer",
     "LocalKeyPair",
+    "SignatureCheck",
     "SignatureEnvelope",
     "SignaturePayload",
+    "SignatureVerifier",
+    "SignerProvider",
     "TamperCheckResult",
     "check_artifact_digest",
     "check_mbom_digest",
     "check_signature_bytes",
     "generate_keypair",
+    "key_fingerprint",
+    "load_envelope",
     "load_private_key",
     "load_public_key",
     "normalize_fingerprints",
     "public_key_fingerprint",
     "save_keypair",
     "sign_artifact",
+    "sign_with_provider",
+    "verifier_registry",
     "verify_artifact",
+    "verify_envelope_signature",
 ]
